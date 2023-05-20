faf4fff0c4c19894dfdb73e0ef2a8ac5564acc68
madewgn <madewg.n2@gmail.com>
2023-05-20 22:42:32 +0000

    xxxx

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+import subprocess
+
+
+
+def vmess(u,exp):
+
+
+# Menjalankan perintah "add-ws" dengan subprocess
+    process = subprocess.Popen(["add-ws"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+    # Mengambil input dari pengguna
+    username = input("username: ")
+    hari = input("exp: ")
+
+    # Mengirim input ke perintah "add-ws"
+    input_text = f"{username}\n{hari}\n"
+    output, errors = process.communicate(input=input_text)
+
+    # Menampilkan output
+    print(output)
+
+# Menampilkan pesan kesalahan jika ada
+    if errors:
+        print("Error:", errors)
+if __name__ == "__main__":
+    vmess("ydzyyd","1")
